62fcd07859a5e8f533189b7f86a5ebdc33cd4efa
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-10-30 20:46:31 +0000

    init

---
+++ components/Tutorial.vue
@@ -0,0 +1,14 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <h1 class="text-3xl font-semibold text-center">
+      WORKOUT TRACKER APP COMING SOON
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuxtTutorial",
+};
+</script>
