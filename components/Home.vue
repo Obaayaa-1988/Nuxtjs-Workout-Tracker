22c3bba88aa4d3aea807ba314ff839cf4562778e
Abigail <ashbella333@gmail.com>
2022-11-01 17:02:29 +0000

    added tailwind components for sidebar and footer

---
+++ components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex min-h-screen">
+    <div class="w-64 border-r border-gray-20">
+      <Navigation />
+    </div>
+
+    <div class="flex-1">
+      <div>
+        <main class="text-center">MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+        <main>MY MAIN CONTENT</main>
+      </div>
+      <FooterTracker />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  // components: { FooterTracker }
+};
+</script>
+
+<style>
+</style>
--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,7 +1,7 @@
 <!-- Please remove this file from your project -->
 <template>
   <div>
-    <h1 class="text-3xl font-semibold text-center">
+    <h1 class="text-3xl font-semibold text-center text-red-500">
       WORKOUT TRACKER APP COMING SOON
     </h1>
   </div>
