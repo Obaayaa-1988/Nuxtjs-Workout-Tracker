beb629f515cfdeb964fcbe7433f6310665f28ba2
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-11-04 23:57:41 +0000

    added background color for sidebar

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -3,7 +3,7 @@
    
 <aside class="w-64" aria-label="Sidebar">
    <!-- <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800 pb-52"> -->
-   <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800 ">
+   <div class="overflow-y-auto py-4 px-3 bg-purple-100 rounded dark:bg-gray-800 ">
       <ul class="space-y-2">
          <li>
             <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
