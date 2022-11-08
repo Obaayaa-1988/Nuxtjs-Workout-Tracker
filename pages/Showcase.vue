396b35832ef1a6bf478fa5240fc35bb56ede448c
Abigail <ashbella333@gmail.com>
2022-11-08 13:54:27 +0000

    created a showcase component

---
+++ pages/Showcase.vue
@@ -0,0 +1,37 @@
+<template>
+  <div> 
+    <div class="
+          h-1/4
+          w-full
+          bg-indigo-200
+          rounded-md
+          bg-clip-padding
+          backdrop-filter backdrop-blur-sm
+          bg-opacity-40
+          border border-gray-100
+        ">
+        <div class="flex justify-around">
+          <h2 class="text-gray-700 text-4xl font-extrabold font-mono pt-28">
+            CHOOSE A WORKOUT
+          </h2>
+          <div></div>
+
+          <div>
+            <img src="images/removebg.png" alt="PanCake" class="w-96 h-96 object-cover" />
+          </div>
+        </div>
+      </div>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
