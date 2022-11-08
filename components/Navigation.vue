daa10ea552363ced2374f1510c77f24b4ce45109
Abigail <ashbella333@gmail.com>
2022-11-08 11:37:51 +0000

    added new styling to sidebar navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,10 +1,10 @@
 <template>
-   <div class="bg-gray-100 h-full">
+   <div class="bg-indigo-200 h-full">
       <aside class="w-64" aria-label="Sidebar">
          <nav class="text-right">
-            <div class="flex justify-between items-center">
-               <h1 class="font-semibold upppercase p-4">
-                  <a href="#" class="hover:text-gray-700">WORKOUT TRACKER</a>
+            <div class="flex justify-evenly items-center">
+               <h1 class="font-semibold upppercase p-8  text-center">
+                  <a href="#" class="hover:text-gray-700 ">MYFIT TRACKER</a>
                </h1>
                <div class="px-4 cursor-pointer md:hidden" id="burger">
                   <svg class="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -14,25 +14,27 @@
                </div>
             </div>
 
-            <div class="flex flex-col items-center gap-y-2 mt-8">
-               <p class="w-24  pr-2">
-                  <a href="#" class="flex justify-between">
-                     <span class="">Home</span>
+            <div class="flex flex-col items-center gap-y-12 mt-8">
+               <p class="w-24  pr-8">
+                  <a href="#" class="flex justify-between gap-4">
+                     <span class="text-lg font-semibold font-mono ">Profile</span>
                      <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                           stroke="currentColor" stroke-width="2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                           class="w-7 h-7 font-semibold ">
                            <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
+
+                       
                      </span>
                   </a>
                </p>
 
-               <p class="w-24 pr-2 border-r-4 border-white">
-                  <a class="flex justify-between">
-                     <span>About</span>
+               <p class="w-24 pr-2 ">
+                  <a class="flex justify-between gap-4">
+                     <span class="text-lg font-semibold font-mono">Notify</span>
                      <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 " fill="none" viewBox="0 0 24 24"
                            stroke="currentColor" stroke-width="2">
                            <path stroke-linecap="round" stroke-linejoin="round"
                               d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
@@ -41,51 +43,43 @@
                   </a>
                </p>
 
-               <p class="w-24 pr-2 border-r-4 border-white">
-                  <a class="flex justify-between">
-                     <span>Contact</span>
+               <p class="w-24 pr-2 ">
+                  <a class="flex justify-between gap-4">
+                     <span class="text-lg font-semibold font-mono">Support</span>
                      <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                           stroke="currentColor" stroke-width="2">
-                           <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
-                        </svg>
+                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                        class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                           d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                     </svg>
+ 
                      </span>
                   </a>
                </p>
-               <p class="w-24 pr-2 border-r-4 border-white">
-                  <a class="flex justify-between">
-                     <span>Contact</span>
+               <p class="w-24 pr-2 ">
+                  <a class="flex justify-between gap-4">
+                     <span class="text-lg font-semibold font-mono">Settings</span>
                      <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                           stroke="currentColor" stroke-width="2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                           class="w-6 h-6 ">
                            <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
+                              d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
+                           <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
+
                      </span>
                   </a>
                </p>
-               <p class="w-24 pr-2 border-r-4 border-white">
-                  <a class="flex justify-between">
-                     <span>Contact</span>
+               <p class="w-24 pr-2 pt-72">
+                  <a class="flex justify-between gap-4">
+                     <span class="text-lg font-semibold font-mono">LogOut</span>
                      <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                           stroke="currentColor" stroke-width="2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                           class="w-6 h-6">
                            <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
+                              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
                         </svg>
-                     </span>
-                  </a>
-               </p>
-               <p class="w-24 pr-2 border-r-4 border-white">
-                  <a class="flex justify-between">
-                     <span>Contact</span>
-                     <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                           stroke="currentColor" stroke-width="2">
-                           <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
-                        </svg>
+
                      </span>
                   </a>
                </p>
