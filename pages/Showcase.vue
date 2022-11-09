3b85350481b1cfa08d945bf8e1ee0169d26de675
Abigail <ashbella333@gmail.com>
2022-11-09 13:31:18 +0000

    added new styling to tracker app components

--- pages/Showcase.vue
+++ pages/Showcase.vue
@@ -16,31 +16,31 @@
         ">
             <div class="flex justify-around mb:block">
                 <div>
-                    <img src="images/removebg.png" alt="PanCake" class="w-full h-full object-cover" />
+                    <img src="images/freework-removebg.png" alt="PanCake" class="w-full h-full object-cover" />
                 </div>
 
 
                 <div>
                     <div>
-                        <h2 class="text-gray-700 text-4xl font-extrabold font-mono pt-28 pb-20">
-                            WELCOME TO MYFIT
+                        <h2 class="text-neutral-700 text-5xl font-extrabold font-mono mt-48 pb-20">
+                            WELCOME TO MYFIT APP
                         </h2>
                     </div>
-                    <div>
+                    <!-- <div>
                         <nuxt-link to="/"
                             class="text-primary ml-2 rounded py-4 px-10  text-xl font-bold cursor-pointer tracking-wider border-primary md:border-2 hover:bg-blue-300 hover:text-white transition ease-out duration-500 bg-pink-600 font-mono outline-none text-white">
                             Get Started</nuxt-link>
-                    </div>
+                    </div> -->
 
                 </div>
 
                 <!------------------------sign up buttons section-->
             </div>
 
-            <div >
+            <div class="mt-16">
                 <div class="text-center">
                     <nuxt-link to="/"
-                        class="text-primary ml-2 rounded-lg py-5 px-32  text-xl font-bold cursor-pointer tracking-wider border-primary md:border-2  hover:text-black transition ease-out duration-500 font-mono outline-none bg-white mb-8">
+                        class="text-primary ml-2 rounded-lg py-5 px-32  text-xl font-bold cursor-pointer tracking-wider border-primary md:border-2  hover:text-black transition ease-out duration-500 font-mono outline-none bg-pink-600 mb-8">
                         Log In With Google</nuxt-link>
                 </div>
                 <br>
@@ -48,8 +48,15 @@
                 <br>
                 <div class="text-center" >
                     <nuxt-link to="/"
-                        class="text-primary ml-2 rounded-lg py-5 px-28  text-xl  cursor-pointer tracking-wider border-primary md:border-2 hover:bg-blue-300 hover:text-white transition ease-out duration-500 bg-black font-mono outline-none text-white">
+                        class="text-primary ml-2 rounded-lg py-5 px-28  text-xl  cursor-pointer tracking-wider border-primary md:border-2 hover:bg-black hover:text-white transition ease-out duration-500 bg-blue-300 font-mono outline-none text-white">
                         Log In With FaceBook</nuxt-link>
+                </div>
+                <br>
+                <br>
+                <div class="text-center" >
+                     <span >
+                        Not A User?<nuxt-link to="/" class="text-cyan-500"> Sign Up</nuxt-link>
+                    </span>
                 </div>
             
             </div>
