a8d42885da20ad69d92d7a40ebcdf8f8e1d74aae
Abigail <ashbella333@gmail.com>
2022-11-14 16:24:04 +0000

    added layout implementation and new content to arms component

--- pages/ArmsWorkOut.vue
+++ pages/ArmsWorkOut.vue
@@ -1,15 +1,31 @@
 <template>
-  <div> 
-    <h1>arms workout onlyyyyy</h1>
+  <div>
+    <div
+      class=" h-1/4 w-full bg-indigo-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100 shadow-lg shadow-gray-500/50">
+      <div class="flex justify-around">
+        <h2 class="text-gray-700 text-4xl font-extrabold font-mono pt-28">
+          LEG WORKOUT
+        </h2>
+        <div></div>
+
+        <div>
+          <img src="images/arms.png" alt="PanCake" class="w-80 h-80 object-cover" />
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-    name: 'ArmsWorkOut'
+  name: 'ArmsWorkOut',
+  layout: "main"
+
 
 }
 </script>
+
 
 <style>
 
