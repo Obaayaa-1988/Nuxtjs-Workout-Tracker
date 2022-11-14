98b4a3d724b9663be269471e26a83ce52626e414
Abigail <ashbella333@gmail.com>
2022-11-14 16:28:08 +0000

    implemented nuxt layout and added new content to component

--- pages/FullWorkOut.vue
+++ pages/FullWorkOut.vue
@@ -1,16 +1,32 @@
 
 <template>
-  <div> 
-    <h1>full workout onlyyyyy</h1>
+  <div>
+    <div
+      class=" h-1/4 w-full bg-indigo-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100 shadow-lg shadow-gray-500/50">
+      <div class="flex justify-around">
+        <h2 class="text-gray-700 text-4xl font-extrabold font-mono pt-28">
+          LEG WORKOUT
+        </h2>
+        <div></div>
+
+        <div>
+          <img src="images/full.png" alt="PanCake" class="w-80 h-80 object-cover" />
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-    name: 'FullWorkOut'
+  name: 'FullWorkOut',
+  layout: "main"
+
 
 }
 </script>
+
 
 <style>
 
