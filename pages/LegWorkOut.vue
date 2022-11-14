2e3ea0e4c698538a5c6dedc16c34c3c809e10191
Abigail <ashbella333@gmail.com>
2022-11-14 15:53:32 +0000

    added new content to leg component

--- pages/LegWorkOut.vue
+++ pages/LegWorkOut.vue
@@ -1,5 +1,18 @@
 <template>
-  <div> 
+  <div>
+      <div
+        class=" h-1/4 w-full bg-indigo-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100 shadow-lg shadow-gray-500/50">
+        <div class="flex justify-around">
+          <h2 class="text-gray-700 text-4xl font-extrabold font-mono pt-28">
+            LEG WORKOUT
+          </h2>
+          <div></div>
+      
+          <div>
+            <img src="images/legs.png" alt="PanCake" class="w-96 h-96 object-cover" />
+          </div>
+        </div>
+      </div>
     
   </div>
 </template>
