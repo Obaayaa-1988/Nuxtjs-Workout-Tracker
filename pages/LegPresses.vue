4c56dc8aa01c663a07d92da0292d1dacebb7608e
Abigail <ashbella333@gmail.com>
2022-11-16 16:53:12 +0000

    created leg press component

---
+++ pages/LegPresses.vue
@@ -0,0 +1,24 @@
+<template>
+  <div> 
+    <section > 
+        <div class="flex justify-center py-8"> 
+            <img src="images/removebg.png" alt="PanCake" class="w-96 h-96 object-cover fill-blue-600" />
+        </div>
+
+    </section>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: "LegPresses",
+    layout: "main"
+
+}
+</script>
+
+<style>
+
+</style>
