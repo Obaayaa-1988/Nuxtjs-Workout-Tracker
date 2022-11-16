8e7f823c581a095ea82db9e0a170796f60bbd76f
Abigail <ashbella333@gmail.com>
2022-11-16 17:16:49 +0000

    created navigate layout

---
+++ layouts/navigate.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex h-screen">
+        <NavigateWorkout />
+
+        <div class="flex-1 overflow-hidden flex flex-col ">
+            <div class="flex-1 overflow-y-auto">
+                <Nuxt />
+
+            </div>
+            <div>
+                <FooterTracker />
+
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+import NavigateWorkout from '~/components/NavigateWorkout.vue';
+import FooterTracker from '~/components/FooterTracker.vue';
+
+export default {
+    name: 'MainLayout',
+    components: {
+        NavigateWorkout,
+        FooterTracker
+    }
+
+}
+</script>
+
+<style>
+
+</style>
