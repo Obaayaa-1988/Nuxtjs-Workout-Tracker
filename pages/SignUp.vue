ee6af3a8bc1574e96ff3ca949a0a79b4981016a4
Abigail <ashbella333@gmail.com>
2022-11-16 14:09:06 +0000

    route sign in to home component

--- pages/SignUp.vue
+++ pages/SignUp.vue
@@ -48,12 +48,15 @@
                                 </div>
             
                                 <!-- Submit button -->
-                                <button type="submit"
-                                    class="inline-block px-7 py-3 bg-gradient-to-r from-green-400 to-blue-500  text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-                                    data-mdb-ripple="true" data-mdb-ripple-color="light">
-                                    Sign in
-                                </button>
-            
+                                <nuxt-link to="/"> 
+                                    <button type="submit"
+                                        class="inline-block px-7 py-3 bg-gradient-to-r from-green-400 to-blue-500  text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                                        data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                        Sign in
+                                    </button>
+                
+                                    
+                                </nuxt-link>
                 
                                 
                             </form>
