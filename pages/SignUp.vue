7725729c60f5b90beef80d034bc6be429acccd6d
Abigail <ashbella333@gmail.com>
2022-11-16 13:34:34 +0000

    changed submit button bg color

--- pages/SignUp.vue
+++ pages/SignUp.vue
@@ -13,6 +13,11 @@
                                 <div class="mb-6">
                                     <input type="text"
                                         class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                        placeholder="Username" />
+                                </div>
+                                <div class="mb-6">
+                                    <input type="text"
+                                        class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                         placeholder="Email address" />
                                 </div>
             
@@ -25,13 +30,9 @@
                                 <div class="mb-6">
                                     <input type="password"
                                         class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                        placeholder="Password" />
+                                        placeholder=" Confirm Password" />
                                 </div>
-                                <div class="mb-6">
-                                    <input type="password"
-                                        class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                        placeholder="Password" />
-                                </div>
+                                
             
                                 <div class="flex justify-between items-center mb-6">
                                     <div class="form-group form-check">
