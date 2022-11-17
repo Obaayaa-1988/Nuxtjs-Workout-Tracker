8712f6065dec1dd9448f1e633aa8cf8a0e07cc60
Abigail <ashbella333@gmail.com>
2022-11-17 12:15:33 +0000

    created one leg squat component

---
+++ pages/OneLegSquat.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <section>
+            <div class="bg-[url('/static/images/lady.jpg')]">
+               
+            </div>
+
+
+            <!-- <div class="flex justify-center py-8"> 
+            <img src="images/out-removebg.png" alt="PanCake" class="w-96 h-96 object-cover fill-blue-600" />
+        </div> -->
+
+        </section>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "OneLegSquat",
+    layout: "main"
+
+}
+</script>
+
+<style>
+
+</style>
