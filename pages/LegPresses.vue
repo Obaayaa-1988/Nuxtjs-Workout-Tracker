31041f73b163ca479c8d0adf79838fad206b10c0
Abigail <ashbella333@gmail.com>
2022-11-17 11:11:57 +0000

    changed leg press component image

--- pages/LegPresses.vue
+++ pages/LegPresses.vue
@@ -2,7 +2,7 @@
   <div> 
     <section > 
         <div class="flex justify-center py-8"> 
-            <img src="images/removebg.png" alt="PanCake" class="w-96 h-96 object-cover fill-blue-600" />
+            <img src="images/out-removebg.png" alt="PanCake" class="w-96 h-96 object-cover fill-blue-600" />
         </div>
 
     </section>
