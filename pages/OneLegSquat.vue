5bffd69aadb89720ddeaae65df6601f3035c4991
Abigail <ashbella333@gmail.com>
2022-11-21 14:17:01 +0000

    added new content for eg squat component

--- pages/OneLegSquat.vue
+++ pages/OneLegSquat.vue
@@ -1,18 +1,84 @@
 <template>
     <div>
-        <section>
-            <!-- <div class="bg-[url('/static/images/lady.jpg')]">
-               
-            </div> -->
+    <div>
+        <section class="h-screen bg-gradient-to-r  from-cyan-400 to-pink-400 ">
+            <div class="container px-6 py-12 h-full">
+                <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+                    <div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+                        <p class="text-white font-extrabold text-xl font-mono">{{new Date()}}</p>
+                        <img src="images/remove-squat.png" />
+                    </div>
+                    <div class="md:w-8/12 lg:w-5/12 lg:ml-20">
+                        <div class="flex justify-center py-8">
+                            <h1 class="text-xl font-semibold pt-8 font-mono text-white">ONE LEG WORKOUT SQUATS SETS AND REPS </h1>
+                        
+                        </div>
+
+                    <div class="mt-8 pb-24 ">
+                        <nuxt-link to="/OneLegSquat">
+                            <div
+                                class="bg-white w-1/2 py-8 rounded overflow-hidden shadow-md relative hover:shadow-lg shadow-gray-500/50 cursor-pointer mb-8 mx-auto">
+                                <div class="flex justify-between">
+                                    <div class="text-gray-600 ml-4 text-xl font-extrabold font-mono "> One Leg Squat</div>
+                    
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-8 h-8 fill-cyan-600 mr-8">
+                                            <path
+                                                d="M500.5 231.4l-192-160C287.9 54.3 256 68.6 256 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2zm-256 0l-192-160C31.9 54.3 0 68.6 0 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2z" />
+                                        </svg>
+                                    </div>
+                    
+                                </div>
+                    
+                            </div>
+                        </nuxt-link>
+                    
+                    
+                        <nuxt-link to="/cardio">
+                            <div
+                                class="bg-white  w-1/2 py-8 rounded overflow-hidden shadow-md relative hover:shadow-lg shadow-gray-500/50 cursor-pointer  mb-8 mx-auto">
+                                <div class=" flex justify-between">
+                                    <div class="text-gray-600 ml-4 text-xl font-extrabold font-mono ">Machine Leg Extentions</div>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-8 h-8 fill-cyan-600 mr-8">
+                                            <path
+                                                d="M500.5 231.4l-192-160C287.9 54.3 256 68.6 256 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2zm-256 0l-192-160C31.9 54.3 0 68.6 0 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2z" />
+                                        </svg>
+                                    </div>
+                    
+                    
+                                </div>
+                    
+                            </div>
+                        </nuxt-link>
+                        <nuxt-link to="/LegPresses">
+                            <div
+                                class="bg-white w-1/2 py-8 rounded overflow-hidden shadow-md relative hover:shadow-lg shadow-gray-500/50 cursor-pointer mx-auto ">
+                                <div class=" flex justify-between">
+                                    <div class="text-gray-600 ml-4 text-xl font-extrabold font-mono ">Leg Presses</div>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-8 h-8 fill-cyan-600 mr-8">
+                                            <path
+                                                d="M500.5 231.4l-192-160C287.9 54.3 256 68.6 256 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2zm-256 0l-192-160C31.9 54.3 0 68.6 0 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2z" />
+                                        </svg>
+                                    </div>
+                    
+                    
+                                </div>
+                            </div>
+                        </nuxt-link>
+                    
+                    </div>
 
 
-            <!-- <div class="flex justify-center py-8"> 
-            <img src="images/out-removebg.png" alt="PanCake" class="w-96 h-96 object-cover fill-blue-600" />
-        </div> -->
-
+                       
+                    </div>
+                </div>
+            </div>
         </section>
-
-
+    
+    </div>
+ 
     </div>
 </template>
 
