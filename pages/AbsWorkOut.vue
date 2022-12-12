a1b16954cf1c0e18a58242a11df204a2b1284b9e
Abigail <ashbella333@gmail.com>
2022-12-12 16:53:09 +0000

    added box shadow to background color

--- pages/AbsWorkOut.vue
+++ pages/AbsWorkOut.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div
-      class=" h-1/4 w-full bg-indigo-100  ">
+      class=" h-1/4 w-full bg-indigo-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100 shadow-lg shadow-gray-500/50  ">
       <div class="flex justify-around">
         <h2 class="text-gray-700 text-4xl font-extrabold font-mono pt-28">
           ABS WORKOUT
