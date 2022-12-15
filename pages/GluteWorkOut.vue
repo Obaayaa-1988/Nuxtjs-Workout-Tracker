e677b87f0d66780311652b9546f337f51c46db4d
Abigail <ashbella333@gmail.com>
2022-12-15 14:19:54 +0000

    added a new image to content

--- pages/GluteWorkOut.vue
+++ pages/GluteWorkOut.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div
-      class=" h-1/4 w-full bg-indigo-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100 shadow-lg shadow-gray-500/50">
+      class=" h-1/4 w-full bg-indigo-300 rounded-b-3xl   bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100 shadow-lg shadow-gray-500/50">
       <div class="flex justify-around">
         <h2 class="text-gray-700 text-4xl font-extrabold font-mono pt-28">
           GLUTES WORKOUT
@@ -13,6 +13,107 @@
         </div>
       </div>
     </div>
+
+    <div>
+      <section class=" rounded-md ">
+        <div class="container">
+    
+          <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+
+            <div class="md:w-8/12 lg:w-6/12  md:mb-0">
+              <img src="images/butty.jpg" />
+            </div>
+    
+            <div class="md:w-8/12 lg:w-5/12 lg:ml-20 ">
+              <h1 class=" text-3xl font-mono text-center rounded font-extrabold py-4 w-3/4 bg-pink-500 mb-7"> Glutes
+                WORKOUTS</h1>
+    
+              <div>
+                <p class="w-5/6 font-serif leading-10  "> It is a long established fact that a reader will be distracted
+                  by
+                  the readable content of a page when looking at its
+                  layout. </p>
+              </div>
+            </div>
+    
+            
+          </div>
+    
+        </div>
+      </section>
+    </div>
+
+    <section class="bg-sky-100 rounded-t-3xl w-full h-full px-16 py-6 md:col-span-2  ">
+    
+      <!--------start---------current date and stopwatch-->
+      <div class="flex justify-center py-8">
+        <h1 class="text-3xl font-extrabold pt-8 font-mono ">LEG WORKOUT VARIATION</h1>
+    
+      </div>
+    
+      <!-----------------stopwatch end------->
+    
+      <!----------------------start cardio cards-->
+      <div class="mt-8 pb-24 ">
+        <nuxt-link to="/OneLegSquat">
+          <div
+            class="bg-yellow-300  w-2/3 py-8 rounded overflow-hidden shadow-md relative hover:shadow-lg shadow-gray-500/50 cursor-pointer mb-8 mx-auto border-l-8 border-l-orange-500">
+            <div class="flex justify-between">
+              <div class=" ml-4 text-xl font-extrabold font-mono text-white"> Romanain Squat</div>
+    
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-8 h-8 fill-white mr-8">
+                  <path
+                    d="M500.5 231.4l-192-160C287.9 54.3 256 68.6 256 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2zm-256 0l-192-160C31.9 54.3 0 68.6 0 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2z" />
+                </svg>
+              </div>
+    
+            </div>
+    
+          </div>
+        </nuxt-link>
+    
+    
+        <nuxt-link to="/cardio">
+          <div
+            class="bg-cyan-400  w-2/3 py-8 rounded overflow-hidden shadow-md relative hover:shadow-lg shadow-gray-500/50 cursor-pointer  mb-8 mx-auto border-l-8 border-l-orange-500">
+            <div class=" flex justify-between">
+              <div class="text-white ml-4 text-xl font-extrabold font-mono ">DeadLifts</div>
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-8 h-8 fill-white mr-8">
+                  <path
+                    d="M500.5 231.4l-192-160C287.9 54.3 256 68.6 256 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2zm-256 0l-192-160C31.9 54.3 0 68.6 0 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2z" />
+                </svg>
+              </div>
+    
+    
+            </div>
+    
+          </div>
+        </nuxt-link>
+        <nuxt-link to="/LegPresses">
+          <div
+            class="bg-emerald-300  w-2/3 py-8 rounded overflow-hidden shadow-md relative hover:shadow-lg shadow-gray-500/50 cursor-pointer mx-auto border-l-8 border-l-orange-500 ">
+            <div class=" flex justify-between">
+              <div class="text-white ml-4 text-xl font-extrabold font-mono ">Sumo Squats</div>
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-8 h-8 fill-white mr-8">
+                  <path
+                    d="M500.5 231.4l-192-160C287.9 54.3 256 68.6 256 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2zm-256 0l-192-160C31.9 54.3 0 68.6 0 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2z" />
+                </svg>
+              </div>
+    
+    
+            </div>
+          </div>
+        </nuxt-link>
+    
+      </div>
+    
+      <!-------------end cardio cards only-->
+    
+    </section>
+
 
   </div>
 </template>
