82d52514cc55d1f73e31cab2fdd3161b98bafe92
Abigail <ashbella333@gmail.com>
2022-12-15 14:54:59 +0000

    added backward svg icon

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -3,6 +3,12 @@
     <div>
       <div
         class="h-64 w-full  rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100 shadow-lg shadow-gray-500/50  ">
+        <nuxt-link to="/Home">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-10 h-10 font-extrabold cursor-pointer">
+            <path
+              d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+          </svg>
+        </nuxt-link>
         <div class="flex justify-around">
 
           <div>
