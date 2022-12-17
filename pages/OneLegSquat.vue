a3cc6a925691ec68e2c72b6a73201e77b7b313d0
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-12-17 23:49:55 +0000

    added new content to one leg squat component

--- pages/OneLegSquat.vue
+++ pages/OneLegSquat.vue
@@ -1,98 +1,26 @@
 <template>
     <div>
-    <!-- <div>
-        <section class=" bg-gradient-to-r from-sky-500 to-indigo-500  ">
-            <div class="container px-6 py-12 h-full">
-                <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-                    <div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
-                        <p class="text-white font-extrabold text-xl font-mono">{{new Date()}}</p>
-                        <img src="images/remove-squat.png" />
-                    </div>
-                    <div class="md:w-8/12 lg:w-5/12 lg:ml-20">
-                        <div class="flex justify-center py-12">
-                            <h1 class="text-xl font-semibold font-mono text-white">ONE LEG WORKOUT SQUATS SETS AND REPS </h1>
-                        
-                        </div>
+        <div class="flex justify-between py-8"> 
+            <nuxt-link to="/LegWorkout">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-10 h-10 font-extrabold cursor-pointer">
+                    <path
+                        d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+                </svg>
+            </nuxt-link>
 
-                    <div class="mt-8 pb-24 ">
-                            <div class="bg-white w-full py-4 rounded overflow-hidden shadow-md hover:shadow-lg shadow-gray-500/50 cursor-pointer mb-8 mx-auto">
-                                <div class="flex justify-between">
-                                    <div class="text-gray-600 ml-4 text-xl font-extrabold font-mono "> 3 Sets </div>
-                    
-                                    <div class="text-gray-600 text-xl font-extrabold font-mono "> <p> 6 Reps </p></div>
-                                    <div class="text-gray-600 mr-4 text-xl font-extrabold font-mono "><p> 60 Seconds Rest  </p></div>
-                    
-                                </div>
-                    
-                            </div>
-                            <div class="flex justify-center py-6 cursor-pointer">
-                                <div class="bg-white w-56 h-56 rounded-full border-pink-100 border-8 text-center">
-                                
-                            </div>
-                                   
-                                
-                                </div>
+            <div class="font-bold text-2xl font-mono"> {{new Date}}</div>
+            <nuxt-link to="/LegExtension">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512" class="w-10 h-10 font-extrabold cursor-pointer">
+                <path
+                    d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z" />
+            </svg>
+            </nuxt-link>
 
-                                <div class="flex justify-evenly py-8"> 
-                                    <div class="bg-cyan-700 text-white py-2 px-8 rounded-md cursor-pointer font-bold font-mono ">
-                                        reset
-                                    </div>
-                                    <div class="bg-cyan-700  py-2 px-8 rounded-md cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-6 h-6 fill-white">
-                                        <path
-                                            d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
-                                    </svg>
-                                    </div>
-                                    <div class="bg-cyan-700 py-2 px-8 rounded-md cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-6 h-6 fill-white">
-                                        <path
-                                            d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z" />
-                                    </svg>
-                                    </div>
-
-                                </div>
-
-                                <div class="flex justify-center"> 
-                                    <p class="bg-gradient-to-r from-pink-500 to-yellow-500 font-bold py-2 px-10 text-white font-mono cursor-pointer">START WORKOUT</p>
-                                </div>
-
-                            
-
-                           
-
-                            <div> 
-
-                            </div>
-                       
-                    </div>
-
-
-                       
-                    </div>
-                </div>
-            </div>
-        </section>
-    
-    </div> -->
-
-    <!----------------creating new section---------------->
-    <!-- <div>
-        <div
-            class=" h-1/4 w-full bg-gray-50 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100 shadow-lg shadow-gray-500/50">
-            <div class="flex justify-around">
-                <h2 class="text-gray-700 text-4xl font-extrabold font-mono pt-44">
-                    ONE LEG SQUAT
-                </h2>
-                <div></div>
-    
-                <div>
-                    <img src="images/removebg.png" alt="PanCake" class="w-96 h-96 object-cover" />
-                </div>
-            </div>
         </div>
-        </div> -->
 
-    <!----------------creating new section---------------->
+    
+        
     
     </div>
 </template>
